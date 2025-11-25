3778fab9258ac4942227b8df3b6a7cfae3222e9d
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-11-25 16:40:25 +0500

    Task6

---
+++ Tyuiu.MuliavinIM.Sprint2.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MuliavinIM.Sprint2.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
