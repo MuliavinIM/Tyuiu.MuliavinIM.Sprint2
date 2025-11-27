fde9875a1463ec43989969a4ad539b6d831a2935
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-11-27 21:57:41 +0500

    Task7

---
+++ Tyuiu.MuliavinIM.Sprint2.Task7.V5,Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MuliavinIM.Sprint2.Task7.V5.Lib
+{
+    public class DataService : ISprint2Task7V5
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+
+            if ((x >= -0.7)
+                 && (x <= 0.7)
+                 && (y >= Math.Pow(x, 2))
+                 && (y <= Math.Exp(-Math.Abs(x))))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+        }
+    }
+}
